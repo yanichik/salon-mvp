5e60f942f210b7425a74970796a562d03cdea9d7
yanichik <brunshte@gmail.com>
2021-05-17 06:31:35 -0700

    add comment

--- utils/setDateByPrevOrNext.js
+++ utils/setDateByPrevOrNext.js
@@ -1,8 +1,5 @@
+// displays prev or next 30 days for 30-day view. Or prev or next month for monthly view
 module.exports = function setDateByPrevOrNext(query, cookies, start, end){
-	// console.log(query);
-	// console.log(cookies);
-	// console.log(start);
-	// console.log(end);
 	if (query.prevOrNext === 'next' && cookies.viewType === 'thirty-day') {
 		start = new Date((new Date(cookies.startDate)).valueOf() + 30*24*60*60*1000).toLocaleString().split(',')[0]
 		end = new Date((new Date(cookies.endDate)).valueOf() + 30*24*60*60*1000).toLocaleString().split(',')[0]
@@ -12,14 +9,6 @@ module.exports = function setDateByPrevOrNext(query, cookies, start, end){
 		start = new Date((new Date(cookies.startDate)).valueOf() - 30*24*60*60*1000).toLocaleString().split(',')[0]
 		end = new Date((new Date(cookies.endDate)).valueOf() - 30*24*60*60*1000).toLocaleString().split(',')[0]
 	}
-	// if (query.prevOrNext === 'next' && cookies.viewType === 'monthly') {
-	// 	start = new Date((new Date(cookies.startDate)).valueOf() + 30*24*60*60*1000).toLocaleString().split(',')[0]
-	// 	end = new Date((new Date(cookies.endDate)).valueOf() + 30*24*60*60*1000).toLocaleString().split(',')[0]
-	// }
-	// if (query.prevOrNext === 'prev' && cookies.viewType === 'monthly') {
-	// 	start = new Date((new Date(cookies.startDate)).valueOf() - 30*24*60*60*1000).toLocaleString().split(',')[0]
-	// 	end = new Date((new Date(cookies.endDate)).valueOf() - 30*24*60*60*1000).toLocaleString().split(',')[0]
-	// }
 	if (query.prevOrNext === 'next' && cookies.viewType === 'monthly') {
 		start = new Date((new Date(cookies.startDate).getFullYear()), (new Date(cookies.startDate).getMonth())+1, 1)
 			.toLocaleString().split(',')[0];
