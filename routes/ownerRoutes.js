62ac6a2b419a2852340b0b0ffaac8260beb71ec2
yanichik <brunshte@gmail.com>
2021-06-05 06:30:49 -0700

    RE-FORMAT: removed calendar & consolidated new transaction & reports & clients into 'transactions'

--- routes/ownerRoutes.js
+++ routes/ownerRoutes.js
@@ -39,77 +39,8 @@ router.post('/transactions', isLoggedIn, async (req, res, next) => {
 	res.render('dashboards/owner/transactions/show', {transaction});
 })
 
-// Option 1: use GET method to pass in user's date range to view Reports
-// LOOK INTO: underscore or lodash util libs 
-// NEED TO ADD: datepicker
+
 router.get('/transactions', isLoggedIn, async (req, res, next) => {
-	if (!Object.keys(req.query).length) {
-		res.cookie('viewType', 'all');
-	}else if(req.query.viewType != undefined){
-		res.cookie('viewType', req.query.viewType);
-	}
-	const user = await User.findOne({email: req.session.passport.user});
-// Default Dates Start: when first opening reports, sets defaults to view all transactions
-	let {startDate, endDate} = req.cookies;
-	// if startDate left blank, set to start of 1900
-	[startDate, endDate] = setDateAtFirstVisit(startDate, endDate)
-	// endDate = setDateAtFirstVisit(startDate, endDate)[1];
-// Default Dates End
-
-// Toggle View Start: toggle between 30-day, monthly, or all transactions views
-// setDateByViewType function: checks if user toggled any of these by checking the query
-	let {viewType, prevOrNext} = req.query;
-	startDate = setDateByViewType(req.cookies, viewType, startDate, endDate)[0];
-	endDate = setDateByViewType(req.cookies, viewType, startDate, endDate)[1];
-	// console.log(startDate, endDate);
-// Toggle View End
-
-	res.cookie('startDate', startDate);
-	res.cookie('endDate', endDate);
-
-// Prev or Next Start: handles dates based on queries of prev or next by user
-	[startDate, endDate] = setDateByPrevOrNext(req.query, req.cookies, startDate, endDate);
-	res.cookie('startDate', startDate);
-	res.cookie('endDate', endDate);
-// Prev or Next End
-	
-	// console.log(`${user.firstName} ${user.lastName}`);
-	// filter inside mongoDB & return filtered + sorted (descending) data per user's date range input
-	let sortedTransactions = await Transaction.find({
-		owner: user._id,
-    date: {
-        $gte: new Date(startDate),
-        $lt:  new Date(endDate)
-    }
-	}).sort({
-		date: -1
-	})
-
-	let viewTotal = sortedTransactions.reduce((acc, v) => {
-		return acc + v.total;
-	}, 0);
-
-	// if startDate is left blank pass in the date of the first transaction
-	// benefits user: date is informative and not arbitrary
-	if (sortedTransactions.length > 0 && startDate === '1/1/1900') {
-		startDate = sortedTransactions[sortedTransactions.length-1].date.toLocaleString().split(',')[0];
-		res.cookie('startDate', startDate);
-	}
-	// window.history.pushState({'blankQuery': ''}, '', '/owner/transactions');
-	// this.window.history.back();
-	res.render('dashboards/owner/transactions/index', {user, sortedTransactions, startDate, endDate, viewTotal});
-})
-
-// Option 2: use PUT method to pass in user's date range to view Reports
-// router.put('/transactions', async (req, res, next) => {
-// 	const {startDate, endDate} = req.body;
-// 	const seededTransactions = await Transaction.find({});
-// 	const sortedTransactions = sortTransactions(seededTransactions, startDate, endDate);
-// 	res.render('dashboards/owner/transactions/index', {sortedTransactions, startDate, endDate});
-// 	// res.send(req.body);
-// })
-
-router.get('/transactions/clients', isLoggedIn, async (req, res, next) => {
 	if (!Object.keys(req.query).length) {
 		res.cookie('viewType', 'all');
 	}else if(req.query.viewType != undefined){
@@ -132,7 +63,6 @@ router.get('/transactions/clients', isLoggedIn, async (req, res, next) => {
 	let clientName = req.query.client;
 	startDate = setDateByViewType(req.cookies, viewType, startDate, endDate)[0];
 	endDate = setDateByViewType(req.cookies, viewType, startDate, endDate)[1];
-	// console.log(startDate, endDate);
 // Toggle View End
 
 	res.cookie('startDate', startDate);
@@ -144,7 +74,6 @@ router.get('/transactions/clients', isLoggedIn, async (req, res, next) => {
 	res.cookie('endDate', endDate);
 // Prev or Next End
 	
-	// console.log(`${user.firstName} ${user.lastName}`);
 	// filter inside mongoDB & return filtered + sorted (descending) data per user's date range input
 	if (req.query.client != undefined && req.query.client != 'all') {
 		clientName = req.query.client;
@@ -187,10 +116,9 @@ router.get('/transactions/clients', isLoggedIn, async (req, res, next) => {
 		startDate = sortedTransactions[sortedTransactions.length-1].date.toLocaleString().split(',')[0];
 		res.cookie('startDate', startDate);
 	}
-	// window.history.pushState({'blankQuery': ''}, '', '/owner/transactions');
-	// this.window.history.back();
-	res.render('dashboards/owner/transactions/clients/index', {clientName, user, sortedTransactions, startDate, endDate, viewTotal});
+	res.render('dashboards/owner/transactions/index', {clientName, user, sortedTransactions, startDate, endDate, viewTotal});
 })
+
 
 router.get('/transactions/:id', isLoggedIn, async (req, res, next) => {
 	const transaction = await Transaction.findById(req.params['id'])
